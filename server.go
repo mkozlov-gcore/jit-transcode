8fdbaef3fa39cc6f330afe538007aa6b9d65d2ee
maximk <650465@gmail.com>
2026-04-28 12:07:55 +0300

    feat: add Cache-Control: no-store header to all responses

--- server.go
+++ server.go
@@ -16,8 +16,9 @@ var videoSegmentPath = regexp.MustCompile(`^/video_\d+\.ts$`)
 
 func withCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-store")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Range")
 		w.Header().Set("Access-Control-Expose-Headers", "X-Transcode-Duration, Content-Length, Content-Range")
 		if r.Method == http.MethodOptions {
