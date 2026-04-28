6e77bc240590ea5c1be3aedf9964e1e9ea5c776c
maximk <650465@gmail.com>
2026-04-28 14:07:00 +0300

    chore: update Cache-Control header and add .dockerignore

--- server.go
+++ server.go
@@ -16,7 +16,7 @@ var videoSegmentPath = regexp.MustCompile(`^/video_\d+\.ts$`)
 
 func withCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Cache-Control", "no-store")
+		w.Header().Set("Cache-Control", "no-cache, no-store")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Range")
